46bff942a8ae2574022156224a0cff77bfabd6c4
Zikai Zhang <zikaizhang@gmail.com>
2013-05-19 17:35:09 +0800

    use cdn

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,7 +14,28 @@ requirejs.config({
 	async: 'require.async',
         template: '../../template',
         anim: '../anim',
-        raphael: 'raphael',
+        raphael: [
+          //'//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min',
+          'raphael',
+      ],
+      jquery: [
+	 '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
+	 '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
+	 'jquery',
+      ],
+      backbone: [
+        '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
+        'backbone',
+      ],
+      bootstrap: [
+        '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.1/bootstrap.min',
+        'bootstrap',
+      ],
+      underscore: [
+        '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
+        'underscore',
+      ],
+        
     },
     map:{
 	"*": {
