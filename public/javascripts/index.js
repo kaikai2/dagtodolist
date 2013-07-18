0e9fd3ef4173d528c97a243d53404cdd26ce1884
Zikai Zhang <zikaizhang@gmail.com>
2013-07-18 11:09:41 +0800

    fix bootstrap-datetimepicker load sequence

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,46 +8,51 @@ requirejs.config({
     //never includes a ".js" extension since
     //the paths config could be for a directory.
     paths: {
-	app: '../app',
-	view: '../view',
-	model: '../model',
-	async: 'require.async',
+        app: '../app',
+        view: '../view',
+        model: '../model',
+        async: 'require.async',
         template: '../../template',
         anim: '../anim',
         raphael: [
           //'//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min',
           'raphael',
-      ],
-      jquery: [
-	 '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
-	 '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
-	 'jquery',
-      ],
-      backbone: [
-        '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
-        'backbone',
-      ],
-      bootstrap: [
-        '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.1/bootstrap.min',
-        'bootstrap',
-      ],
-      underscore: [
-        '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
-        'underscore',
-      ],
-        
+        ],
+        jquery: [
+            '//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min',
+            '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
+            'jquery',
+        ],
+        backbone: [
+            '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
+            'backbone',
+        ],
+        bootstrap: [
+            '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.1/bootstrap.min',
+            'bootstrap',
+        ],
+        underscore: [
+            '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
+            'underscore',
+        ],
+        'bootstrap-datetimepicker': [
+            '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/0.0.11/js/bootstrap-datetimepicker.min',
+            'bootstrap-datetimepicker.min',
+        ],
+        smart: [
+            'smart-2.9.min',
+        ],
     },
     map:{
-	"*": {
-	    'smart': 'smart-2.9.min',
-        'bootstrap-datetimepicker': 'bootstrap-datetimepicker.min',
-	},
+        "*": {
+        },
     },
     shim: {
-	underscore: {exports: '_'},
-	backbone: {deps:["underscore", "jquery"], exports: "Backbone"},
-    bootstrap: {deps: ["jquery"]},
-    "bootstrap-datetimepicker": {deps: ["jquery", "bootstrap"]},
+        underscore: {exports: '_'},
+        backbone: {deps:["underscore", "jquery"], exports: "Backbone"},
+        bootstrap: {deps: ["jquery"]},
+        "bootstrap-datetimepicker": {deps: ["bootstrap"]},
+        "bootstrap-datetimepicker.min": {deps: ["bootstrap"]},
     }
 });
 requirejs(['underscore', 'smart', 'raphael.amd', 'app/app'], function(_,s){
